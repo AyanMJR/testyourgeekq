56bf97e562cf5c6e991b4f264b92e610b249b51a
Ayan Majumder <ayan.majumder48@gmail.com>
2024-12-18 15:03:30 +0530

    Fix: Import error

--- common/components/molecules/GuestSignIn/index.tsx
+++ common/components/molecules/GuestSignIn/index.tsx
@@ -1,14 +1,14 @@
 "use client";
 import { homepage } from "@/common/constant/staticText";
 import { Button3D } from "@/common/components/atoms";
-import { Arrow } from "@/common/components/icons/Arrow";
+import { ArrowEnterIcon } from "@/common/components/icons";
 import { useFormStatus } from "react-dom";
 
 export const GuestSignIn = () => {
   const { pending } = useFormStatus();
   return (
     <Button3D
-      icon={<Arrow />}
+      icon={<ArrowEnterIcon />}
       label={homepage.guestButton}
       loading={pending}
       type="submit"
